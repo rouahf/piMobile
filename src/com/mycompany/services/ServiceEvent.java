41f2510eaeb3b8d9cac9ce598ccc9a8d197b9c5b
hamza_bargougui <hamza.bargougui@esprit.tn>
2023-05-11 03:25:33 +0100

    Crud+stats+map

--- src/com/mycompany/services/ServiceEvent.java
+++ src/com/mycompany/services/ServiceEvent.java
@@ -5,8 +5,6 @@
  */
 package com.mycompany.services;
 
-
-import Utils.Statics;
 import com.codename1.io.CharArrayReader;
 import com.codename1.io.ConnectionRequest;
 import com.codename1.io.JSONParser;
@@ -15,97 +13,109 @@ import com.codename1.io.NetworkManager;
 import com.codename1.ui.TextSelection.Char;
 import com.codename1.ui.events.ActionListener;
 import com.mycompany.entities.Event;
+import com.mycompany.utilis.Statics;
 import java.util.ArrayList;
 import java.util.Map;
 //manuelle
 import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
+//apparemment codename on yaarech java.sql go figure
+//import java.sql.Connection;
+//import java.sql.DriverManager;
+//import java.sql.PreparedStatement;
+//import java.sql.ResultSet;
+//import java.sql.SQLException;
 import com.codename1.db.Cursor;
 import com.codename1.db.Database;
 import com.codename1.db.Row;
 import com.codename1.io.Log;
+import java.io.IOException;
+import java.util.HashMap;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+
+import com.codename1.io.ConnectionRequest;
+import com.codename1.io.JSONParser;
+import com.codename1.io.NetworkEvent;
+import com.codename1.io.NetworkManager;
+import com.codename1.ui.events.ActionEvent;
+import com.codename1.ui.events.ActionListener;
 import java.io.ByteArrayInputStream;
 import java.io.InputStreamReader;
+
 //fin
-
-
 /**
  *
  * @author mizoj
  */
 public class ServiceEvent {
-    
-     //singleton
-    public static ServiceEvent instance = null ;
-    
-     public static boolean resultOk = true;
+
+    //singleton
+    public static ServiceEvent instance = null;
+
+    public static boolean resultOk = true;
     //initilisation connection request
-    private ConnectionRequest req ;
-    
-     public static ServiceEvent getInstance() {
-     if (instance == null)
-             instance = new ServiceEvent ();
-         return instance;
-     }
-    
-     
-     public ServiceEvent(){
-     
-     req = new ConnectionRequest();
-     }
-     
-     
-      //ajout 
-    public void ajouter( Event event) {
-        
-        String url = Statics.BASE_URL+"/mobilehamza/event/ajout?nom="+event.getNom()+"&type="+event.getType();
-                
+    private ConnectionRequest req;
+
+    public static ServiceEvent getInstance() {
+        if (instance == null) {
+            instance = new ServiceEvent();
+        }
+        return instance;
+    }
+
+    public ServiceEvent() {
+
+        req = new ConnectionRequest();
+    }
+
+    //ajout 
+    public void ajouter(Event event) {
+
+        String url = Statics.BASE_URL + "/ajout?nom=" + event.getNom() + "&type=" + event.getType();
+
         req.setUrl(url);
         req.addResponseListener((e) -> {
-            
+
             String str = new String(req.getResponseData());//Reponse json hethi lyrinaha fi navigateur 9bila
-            System.out.println("data == "+str);
+            System.out.println("data == " + str);
         });
-        
+
         NetworkManager.getInstance().addToQueueAndWait(req); //execution ta3 request sinon yet3ada chy dima nal9awha
-        
+
     }
-    
+
     //affichage 
-    
-    public ArrayList<Event> affichageEvent()
-    {
-    ArrayList<Event> result = new ArrayList<>();
-    
-    String url = Statics.BASE_URL+"/mobilehamza/event/all";
-    
-    req.setUrl(url);
-    req.addResponseListener(new ActionListener<NetworkEvent>() {
-        @Override
-        public void actionPerformed(NetworkEvent evt) {
-        JSONParser jsonp ;
-        jsonp = new JSONParser();
-        try{
-            Map<String,Object> mapEvent = jsonp.parseJSON(new CharArrayReader(new String(req.getResponseData()).toCharArray()));
-             List<Map<String,Object>> listOfMaps =  (List<Map<String,Object>>) mapEvent.get("root");
-       
-        for(Map<String, Object> obj : listOfMaps) {
+    public ArrayList<Event> affichageEvent() {
+        ArrayList<Event> result = new ArrayList<>();
+
+        String url = Statics.BASE_URL + "/all";
+
+        req.setUrl(url);
+        req.addResponseListener(new ActionListener<NetworkEvent>() {
+            @Override
+            public void actionPerformed(NetworkEvent evt) {
+                JSONParser jsonp;
+                jsonp = new JSONParser();
+                try {
+                    Map<String, Object> mapEvent = jsonp.parseJSON(new CharArrayReader(new String(req.getResponseData()).toCharArray()));
+                    List<Map<String, Object>> listOfMaps = (List<Map<String, Object>>) mapEvent.get("root");
+
+                    for (Map<String, Object> obj : listOfMaps) {
                         Event ev = new Event();
-                        
+
                         //dima id fi codename one float 
                         float id = Float.parseFloat(obj.get("id").toString());
-                        
+
                         String nom = obj.get("nom").toString();
-                        
+
                         String type = obj.get("type").toString();
-                        
-                        ev.setId((int)id);
+
+                        ev.setId((int) id);
                         ev.setNom(nom);
                         ev.setType(type);
-                        
+
 //                        //Date 
 //                        String DateConverter =  obj.get("date").toString().substring(obj.get("date").toString().indexOf("timestamp") + 10 , obj.get("date").toString().lastIndexOf("}"));
 //                        
@@ -114,75 +124,91 @@ public class ServiceEvent {
 //                        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
 //                        String dateString = formatter.format(currentTime);
 //                        ev.setDateEvent(dateString);
-                        
                         //insert data into ArrayList result
                         result.add(ev);
-        
-        
-        
-        
-        }}
-        catch(Exception ex){
-       ex.printStackTrace();}
-        
-           
-           }
-    } );
-    
-      NetworkManager.getInstance().addToQueueAndWait(req);//execution ta3 request sinon yet3ada chy dima nal9awha
-    return result;
-    }
 
-            
-            
-            
-            
-            //Delete 
-    public boolean deleteEvent(int id ) {
-        String url = Statics.BASE_URL +"/mobilehamza/event/delete/"+id;
-        
-        req.setUrl(url);
-        
-        req.addResponseListener(new ActionListener<NetworkEvent>() {
-            @Override
-            public void actionPerformed(NetworkEvent evt) {
-                    
-                    req.removeResponseCodeListener(this);
+                    }
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+
             }
         });
-        
-        NetworkManager.getInstance().addToQueueAndWait(req);
-        return  resultOk;
+
+        NetworkManager.getInstance().addToQueueAndWait(req);//execution ta3 request sinon yet3ada chy dima nal9awha
+        return result;
     }
-            
-            
-            
-            
-               //Update 
-    public boolean modifierEvent(Event event) {
-        String url = Statics.BASE_URL +"/mobilehamza/event/update/"+event.getId()+"?nom="+event.getNom()+"&type="+event.getType();
+
+    //Delete 
+    public boolean deleteEvent(int id) {
+        String url = Statics.BASE_URL + "/delete/" + id;
+
         req.setUrl(url);
-        
+
         req.addResponseListener(new ActionListener<NetworkEvent>() {
             @Override
             public void actionPerformed(NetworkEvent evt) {
-                resultOk = req.getResponseCode() == 200 ;  // Code response Http 200 ok
+
+                req.removeResponseCodeListener(this);
+            }
+        });
+
+        NetworkManager.getInstance().addToQueueAndWait(req);
+        return resultOk;
+    }
+
+    //Update 
+    public boolean modifierEvent(Event event) {
+        String url = Statics.BASE_URL + "/update/" + event.getId() + "?nom=" + event.getNom() + "&type=" + event.getType();
+        req.setUrl(url);
+
+        req.addResponseListener(new ActionListener<NetworkEvent>() {
+            @Override
+            public void actionPerformed(NetworkEvent evt) {
+                resultOk = req.getResponseCode() == 200;  // Code response Http 200 ok
                 req.removeResponseListener(this);
             }
         });
-        
-    NetworkManager.getInstance().addToQueueAndWait(req);//execution ta3 request sinon yet3ada chy dima nal9awha
-    return resultOk;
-        
+
+        NetworkManager.getInstance().addToQueueAndWait(req);//execution ta3 request sinon yet3ada chy dima nal9awha
+        return resultOk;
+
     }
-            
-            
-            
-            
-    
+
+//public Map<String, Integer> getEventStatsByType() {
+//    Map<String, Integer> eventStats = new HashMap<>();
+//     String url = Statics.BASE_URL+"/all";
+//    ConnectionRequest request = new ConnectionRequest(url);
+//
+//    request.addResponseListener(new ActionListener<NetworkEvent>() {
+//        @Override
+//        public void actionPerformed(NetworkEvent evt) {
+//            try {
+//                JSONParser parser = new JSONParser();
+//                Map<String, Object> jsonResponse = parser.parseJSON(new InputStreamReader(
+//                        new ByteArrayInputStream(request.getResponseData()), "UTF-8"));
+//                
+//                 // Add logging statement to check the JSON response
+//                Log.p("JSON Response: " + jsonResponse);
+//                // Assuming your JSON response is an array of objects with 'type' and 'event_count' keys
+//                List<Map<String, Object>> events = (List<Map<String, Object>>) jsonResponse.get("root");
+//                for (Map<String, Object> event : events) {
+//                    String eventType = (String) event.get("type");
+//                    int eventCount = Integer.parseInt(event.get("event_count").toString());
+//                    eventStats.put(eventType, eventCount);
+//                }
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+//        }
+//    });
+//
+//    NetworkManager.getInstance().addToQueueAndWait(request);
+//    return eventStats;
+//}
     public Map<String, Integer> getEventStatsByType() {
     Map<String, Integer> eventStats = new HashMap<>();
-     String url = Statics.BASE_URL+"/mobilehamza/event/all";
+    String url = Statics.BASE_URL + "/all";
     ConnectionRequest request = new ConnectionRequest(url);
 
     request.addResponseListener(new ActionListener<NetworkEvent>() {
@@ -211,13 +237,10 @@ public class ServiceEvent {
     NetworkManager.getInstance().addToQueueAndWait(request);
     return eventStats;
 }
-    
-            
-            }
 
 
 
 
-   
-         
- 
+
+
+}
